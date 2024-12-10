661266b405e6bca33f23e630c80510f167164534
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2024-12-10 16:14:43 +0000

    settings

--- collabproj/settings.py
+++ collabproj/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)xwxgzc(-jeve4l*52z4p+f4diyiwz&o($)j5=t3&lxzh87u-('
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
