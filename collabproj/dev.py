394c0504255768866016979f43e165490e18c882
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2025-01-17 14:31:42 +0000

    created settings files

---
+++ collabproj/dev.py
@@ -0,0 +1,6 @@
+import os
+from datetime import date, timedelta
+
+from .base import *
+
+DEBUG = True
---
+++ collabproj/production.py
@@ -0,0 +1,9 @@
+import os
+from datetime import date, timedelta
+
+from .base import *
+
+DEBUG = False
+
+
+ALLOWED_HOSTS = ['kinorg.com', 'www.kinorg.com']
