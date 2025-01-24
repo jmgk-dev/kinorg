952458880aff497a612050c3f4a4e539d223ae72
Jamie Kingett <jamiekingett@jamies-mbp.home>
2025-01-24 16:51:03 +0000

    updated static url

--- collabproj/settings/prod.py
+++ collabproj/settings/prod.py
@@ -9,6 +9,8 @@ ALLOWED_HOSTS = ['kinorg.com', 'www.kinorg.com', '164.92.66.154']
 
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
+STATIC_URL = '/deploy_static/'
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
