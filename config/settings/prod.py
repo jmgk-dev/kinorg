63ad3c2a86e22f44b6e0132afbfca407851ce028
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2026-01-16 09:00:58 +0000

    switched caching to pymemecache

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -46,7 +46,7 @@ BUGSNAG = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.memcached.PyLibMCCache",
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
         "LOCATION": "127.0.0.1:11211",
     }
 }
