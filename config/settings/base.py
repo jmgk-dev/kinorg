bc9d2a152141a09feefe959d15ed7be92a2d8a30
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2026-02-27 14:05:44 +0000

    added bugsnag to middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'bugsnag.django.middleware.BugsnagMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
