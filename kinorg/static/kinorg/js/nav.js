f119fe3ccc682be608e5d525a741be242ba8a500
Jamie Kingett <jamiekingett@Jamies-MacBook-Pro.local>
2026-03-25 14:46:02 +0000

    Fix scrollbar reflow when opening burger menu
    Measure scrollbar width before hiding overflow and apply it as
    body padding-right so the page content doesn't shift.

--- kinorg/static/kinorg/js/nav.js
+++ kinorg/static/kinorg/js/nav.js
@@ -1,8 +1,10 @@
 function openMenu() {
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    document.body.style.paddingRight = scrollbarWidth + 'px';
+    document.body.style.overflow = 'hidden';
     document.getElementById('nav_menu').classList.add('is-open');
     document.getElementById('nav_backdrop').classList.add('is-open');
     document.getElementById('nav_icon').classList.add('is-open');
-    document.body.style.overflow = 'hidden';
 }
 
 function closeMenu() {
@@ -10,6 +12,7 @@ function closeMenu() {
     document.getElementById('nav_backdrop').classList.remove('is-open');
     document.getElementById('nav_icon').classList.remove('is-open');
     document.body.style.overflow = '';
+    document.body.style.paddingRight = '';
 }
 
 document.getElementById('nav_toggle').onclick = function() {
